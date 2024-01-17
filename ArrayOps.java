3cd6c55a7b22a64fc70e4af449d3d57227a4d066
AbedalsalamAkel <154346157+AbedalsalamAkel@users.noreply.github.com>
2024-01-17 18:13:31 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,53 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        System.out.println(isSorted(new int[] {7, 5, 4, 3, -12}));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
-    }
+            int Sum = (array.length + 1) * (array.length + 2) / 2;
+            for (int i : array) {
+                Sum -= i;
+            }
+            return Sum;
+        }
+        
+    
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
-    }
+            int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
+            for (int value : array) {
+                if (value > max) {
+                    secondMax = max;
+                    max = value;
+                } else if (value > secondMax && value != max) {
+                    secondMax = value;
+                }
+            }
+            return secondMax;
+        }
+        
+    
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+         for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]  ) {
+                        break;
+                    }
+                }
+            }
+            return true;
+        }
+    public static boolean isSorted(int[] array) {
+    for (int i = 0; i < array.length - 1; i++) {
+        if (array[i] < array[i + 1]) {
+            return false;
+        }
     }
-
-    public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
-    }
-
+    return true;
 }
+    }
+        
+    
+
+
