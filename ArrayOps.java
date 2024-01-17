2e78b3e912c06a9f8309c103a28a373822bf1fb5
AbedalsalamAkel <154346157+AbedalsalamAkel@users.noreply.github.com>
2024-01-17 18:29:59 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,49 +2,61 @@ public class ArrayOps {
     public static void main(String[] args) {
     }
     
-    public static int findMissingInt (int [] array) {
-            int Sum = (array.length + 1) * (array.length + 2) / 2;
-            for (int i : array) {
-                Sum -= i;
-            }
-            return Sum;
-        }
+    public static int findMissingInt(int[] array) {
+    int n = array.length + 1;
+    int totalSum = n * (n - 1) / 2;
+    for (int num : array) {
+        totalSum -= num;
+    }
+    return totalSum;
+}
+
         
     
 
-    public static int secondMaxValue(int [] array) {
-            int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
-            for (int value : array) {
-                if (value > max) {
-                    secondMax = max;
-                    max = value;
-                } else if (value > secondMax && value != max) {
-                    secondMax = value;
-                }
-            }
-            return secondMax;
+   public static int secondMaxValue(int[] array) {
+    int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
+    for (int value : array) {
+        if (value > max) {
+            secondMax = max;
+            max = value;
+        } else if (value > secondMax && value != max) {
+            secondMax = value;
         }
+    }
+    // Handling the case where the maximum value appears multiple times
+    return (secondMax == Integer.MIN_VALUE) ? max : secondMax;
+}
+
         
     
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-         for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                if (array1[i] == array2[j]  ) {
-                        break;
-                    }
-                }
+  public static boolean containsTheSameElements(int[] array1, int[] array2) {
+    if (array1.length != array2.length) return false;
+
+    for (int i = 0; i < array1.length; i++) {
+        boolean found = false;
+        for (int j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                found = true;
+                break;
             }
-            return true;
         }
-    public static boolean isSorted(int[] array) {
+        if (!found) return false;
+    }
+    return true;
+}
+
+       
+   public static boolean isSorted(int[] array) {
     for (int i = 0; i < array.length - 1; i++) {
-        if (array[i] < array[i + 1]) {
+        if (array[i] > array[i + 1]) {
             return false;
         }
     }
     return true;
 }
+
     }
         
     
